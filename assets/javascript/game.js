685cae9799900ddb7d4d5c157db6ae0cb96dfba3
SchneiderMan <jschneid94@gmail.com>
2018-06-15 16:36:15 -0700

    Wrote out the pseudocode.

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+// Count number of wins and guesses remaining
+var wins = 0;
+var remainingGuess = 15;
+
+// Create a word bank of words used in game
+
+// Computer chooses word and displays the length with _'s
+
+// Function to restart the game
+function restartGame() {
+    wins = 0;
+    remainingGuess = 15;
+}
+
+
+// When user presses a key...
+    // Replace the prompt text
+
+    // Compare guess with the letters in the word
+        // If guess is correct
+            // Replace a blank space with the letter
+        // If guess is incorrect
+            // Number of guesses decreased by 1
+
+// Check status of the game
+    // If player guesses all the characters correctly...
+        // Win count increases by 1
+        // Computer chooses new word and displays word length
+        // Number of guesses is reset
+    // If number of guesses reaches 0...
+        // Replace prompt text with 'try again' message
+        // Computer chooses new word and displays word length
+        // Number of guesses is reset
+
+// Display number of guesses remaining
+
+// Display letters already guessed
