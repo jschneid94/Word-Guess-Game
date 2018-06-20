62d133e394242724341081a4fc41608919b09e5f
SchneiderMan <jschneid94@gmail.com>
2018-06-20 10:10:52 -0700

    Added some additional text and changed javascript to allow apostrophes to appear in placeholder.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var $losses = document.getElementById('losses');
 
 /* Create variables for game (wordBank, wins, losses, picked word, guesses left,
 game running, picked word placeholder, guessed letter bank, incorrect letter bank)*/
-var wordBank = ["Eleanor Rigby", "Magical Mystery Tour", "Fixing A Hole", "A Day In The Life", "Golden Slumbers", "I Want To Hold Your Hand", "For No One", "Taxman", "All You Need Is Love", "Come Together", "Hey Jude", "Ticket To Ride", "Because"];
+var wordBank = ["Eleanor Rigby", "Magical Mystery Tour", "Fixing A Hole", "A Day In The Life", "Golden Slumbers", "I Want To Hold Your Hand", "For No One", "Taxman", "All You Need Is Love", "Come Together", "Hey Jude", "Ticket To Ride", "Don't Let Me Down"];
 var wins = 0;
 var losses = 0
 var guessesLeft = 8;
@@ -35,7 +35,9 @@ function newGame() {
     for (var i = 0; i < pickedWord.length; i++) {
         if (pickedWord[i] === " ") {
             pickedWordPlaceholderArr.push(" ");
-        } else {
+        } else if (pickedWord[i] === "'") {
+            pickedWordPlaceholderArr.push("'");
+        }else {
             pickedWordPlaceholderArr.push("_");
         }
     }
